d88e5b2f3572fab25396feac50dcb0408f6b63f1
hkaluuma <hkaluuma@idi.co.ug>
2021-09-07 18:40:02 +0300

    Third commit

--- app/src/main/java/com/example/test/AddPatientActivity.java
+++ app/src/main/java/com/example/test/AddPatientActivity.java
@@ -25,8 +25,11 @@ public class AddPatientActivity extends AppCompatActivity {
 
         //make reference to the xml for the spinner
         Spinner spinnerd =  findViewById(R.id.spinner_disease);
+        Spinner spinnercity =  findViewById(R.id.spinner_city);
         //assigning data source
-        String[] disease_array = {"Malaria","COVID19","Yellow Fever","HepatitisB"};
+        String[] disease_array = {"nothing selected","Malaria","COVID19","Yellow Fever","HepatitisB"};
+        String[] city_array = {"nothing selected","Nairobi","Dodoma","Kampala","Kigali","Juba"};
+
         //desinged a spinner item design
         //configure An array adapter
         ArrayAdapter<String> disease_adapter = new ArrayAdapter<String>(AddPatientActivity.this, R.layout.spinner_item_design, R.id.spinner_txv, disease_array);
@@ -41,11 +44,27 @@ public class AddPatientActivity extends AppCompatActivity {
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
-                //selected_disease = "nothing selected";
+                selected_disease = "nothing selected";
 
             }
         });
 
+        //configure our arrayadapter for spinner city
+        ArrayAdapter<String> city_adapter =  new ArrayAdapter<String>(AddPatientActivity.this,R.layout.spinner_item_design,R.id.spinner_txv, city_array);
+        spinnercity.setAdapter(city_adapter);
+
+        spinnercity.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                selected_city = city_array[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                selected_city = "nothing selected";
+
+            }
+        });
         //implementation of button
         //making reference to the button
         buttonsubmit = (Button) findViewById(R.id.register_patient);
@@ -58,25 +77,45 @@ public class AddPatientActivity extends AppCompatActivity {
                 EditText edx_age = findViewById(R.id.edtx_age);
                 EditText edx_contact = findViewById(R.id.edtx_contact);
                 EditText edx_email = findViewById(R.id.edtx_email);
-                EditText edx_password = findViewById(R.id.edtx_password);
+                EditText edx_password = findViewById(R.id.editext_pasword);
 
 
                 //assigning the textfield objetcts to the global variables
                 patientnames = edx_names.getText().toString();
+                //patientnames = edx_names.getText().toString();
+                age = edx_age.getText().toString();
+                contacts = edx_contact.getText().toString();
+                email = edx_email.getText().toString();
+                password = edx_password.getText().toString();
+
 
                 // check if empty
                 if (patientnames.isEmpty()) {
                     edx_names.setError("Patient names are required");
+                }else if(password.isEmpty()){
+                    edx_password.setError("Password is required");
+                }else if(age.isEmpty()){
+                    edx_age.setError("Patient age is required");
+                }else if(contacts.isEmpty()){
+                    edx_contact.setError("Phonenumber is required");
+                }else if(email.isEmpty()){
+                    edx_email.setError("Email is required");
+                }else if(selected_disease.equals("nothing selected")){
+                    Toast.makeText(AddPatientActivity.this, "No disease selected", Toast.LENGTH_SHORT).show();
+                }else if(selected_city.equals("nothing selected")){
+                    Toast.makeText(AddPatientActivity.this, "No city selected", Toast.LENGTH_SHORT).show();
+                }else{
+                    //happens here
+                    //Toast.makeText(AddPatientActivity.this, "Submission successful", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Submission succesful", Toast.LENGTH_SHORT).show();
+                    //navigate between two activities
+                    Intent intentregister = new Intent(AddPatientActivity.this,HomeActivity.class);
+                    startActivity(intentregister);
                 }
 
 
 
-                //happens here
-                //Toast.makeText(AddPatientActivity.this, "Submission successful", Toast.LENGTH_SHORT).show();
-                Toast.makeText(getApplicationContext(), "Submission succesful", Toast.LENGTH_SHORT).show();
-                //navigate between two activities
-                //Intent intentregister = new Intent(AddPatientActivity.this,HomeActivity.class);
-                //startActivity(intentregister);
+
 
             }
         });
--- app/src/main/java/com/example/test/HomeActivity.java
+++ app/src/main/java/com/example/test/HomeActivity.java
@@ -2,13 +2,45 @@ package com.example.test;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class HomeActivity extends AppCompatActivity {
+    //global variables
+    String username, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        //our codes from here
+        Button buttonlogin = findViewById(R.id.login_btn);
+        buttonlogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //we can add our codes
+                //make references to the xml
+                EditText edtx_username = findViewById(R.id.edtx_username);
+                EditText edtx_password = findViewById(R.id.edtx_password);
+
+                //assigning the textfield objects to the global variables
+                username = edtx_username.getText().toString();
+                password = edtx_password.getText().toString();
+
+                if (username.isEmpty()) {
+                    edtx_username.setError("Username is required");
+                }else if(password.isEmpty()) {
+                    edtx_password.setError("Password is required");
+                }else {
+                    Toast.makeText(HomeActivity.this, "Login successful", Toast.LENGTH_SHORT).show();
+                    Intent intentlogin = new Intent(HomeActivity.this, AddPatientActivity.class);
+                    startActivity(intentlogin);
+                }
+            }
+        });
     }
 }
